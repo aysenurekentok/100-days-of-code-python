a3a6be8edd60628817d199126d269e2d909ba583
ayseNur <aysenurekentok@gmail.com>
2022-08-31 01:58:58 +0300

    first commit

--- day-35-rain-sms-alert/main.py
+++ day-35-rain-sms-alert/main.py
@@ -1,14 +1,19 @@
 import requests
 from twilio.rest import Client
+from dotenv import load_dotenv
+import os
 
-account_sid = "AC7f9edd29a4222509228fd33097692275"
-auth_token = "0fe96b5d446744aac5bccd6263f1a312"
+load_dotenv("../../EnvVar/.env.txt")
+
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
+OWM_api_key = os.getenv("OWM_API_KEY")
 
 parameters = {
     "lat": 41.013000,
     "lon": 28.974800,
     "exclude": "current,minutely,daily",
-    "appid": "69f04e4613056b159c2761a9d9e664d2"
+    "appid": OWM_api_key
 }
 
 url = "https://api.openweathermap.org/data/2.5/onecall"
@@ -16,13 +21,16 @@ url = "https://api.openweathermap.org/data/2.5/onecall"
 response = requests.get(url, params=parameters)
 response.raise_for_status()
 
+# Getting the next 12 hours weather data
 hourly_weather = response.json()["hourly"]
 next_12_hours = hourly_weather[:12]
 
 rain = False
 
+# Checking if it's going to rain in the next 12 hours
 for weather in next_12_hours:
     id = weather["weather"][0]["id"]
+    # In weather condition codes below 700 is all rain, snow and thunderstorm
     if id < 700:
         rain = True
 
@@ -32,7 +40,7 @@ if rain:
         .create(
         body="It's going to rain today. Don't forget to take your umbrella.",
         from_='+15038095584',
-        to=''
+        to='+905438576254'
         )
 
     print(message.status)
