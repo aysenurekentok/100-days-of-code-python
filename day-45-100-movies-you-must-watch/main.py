9f9615f28ac26babf156d6e2150a625abc3aa4bf
ayseNur <aysenurekentok@gmail.com>
2022-09-07 23:37:50 +0300

    first commit

---
+++ day-45-100-movies-you-must-watch/main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+response.raise_for_status()
+soup = BeautifulSoup(response.text, "html.parser")
+
+movies = soup.find_all(name="h3", class_="title")
+movies_list = []
+for movie in movies:
+    movie_name = movie.getText().split(" ", 1)[1]
+    movies_list.append(movie_name)
+
+with open("movies.txt", "w", encoding="utf-8") as file:
+    for movie in movies_list[::-1]:
+        file.write(f"{movies_list[::-1].index(movie) + 1}) {movie}\n")
